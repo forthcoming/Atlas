79d53b71f102ca4bba3a4204a1a07dfa93ac308b
xxoome <akatsuki10086@gmail.com>
2017-06-08 15:00:55 +0800

    Create settings.py

--- Collector/settings.py
+++ Collector/settings.py
@@ -28,7 +28,7 @@ LOG_ENCODING = "utf-8"
 LOG_FILE = "log/spider.log"
 LOG_FORMAT = '%(asctime)s [%(name)s] %(levelname)s: %(message)s'
 LOG_DATEFORMAT = "%Y-%m-%d %H:%M:%S"
-LOG_LEVEL = 'INFO'
+LOG_LEVEL = 'DEBUG'
 LOG_STDOUT=False #default false,If True, all standard output (and error) of your process will be redirected to the log
 
 '''
