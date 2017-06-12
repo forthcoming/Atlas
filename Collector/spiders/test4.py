a5854b0da706af68ef7e52a1ae1f45d31d365fb4
xxoome <akatsuki10086@gmail.com>
2017-06-12 23:08:16 +0800

    Create test4.py

--- Collector/spiders/test4.py
+++ Collector/spiders/test4.py
@@ -10,7 +10,7 @@ scheduler.py，dupefilter.py，queue.py和connection.py实现断点续爬,分布
 一旦启动了scrapy_redis.pipelines.RedisPipeline,会将item数据保存到dmoz:item中,通过type dmoz:items可得其类型为list
 item_key(self, item, spider)此处可以自定义item_key
 SCHEDULER_PERSIST = True，中断程序，dmoz:dupefilter和dmoz:requests会存在，程序执行完后还会保存dmoz:dupefilter（此时已不存在dmoz:requests）
-SCHEDULER_PERSIST = False，(默认)中断程序，dmoz:dupefilter和dmoz:requests会存在，程序执行完后会删除dmoz:dupefilter（此时已不存在dmoz:requests）
+SCHEDULER_PERSIST = False，中断程序，dmoz:dupefilter和dmoz:requests会被清空，程序执行完后会删除dmoz:dupefilter（此时已不存在dmoz:requests）
 type dmoz:dupefilter返回set
 type dmoz:requests返回zset
 '''
