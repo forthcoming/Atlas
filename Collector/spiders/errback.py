18aaad0beccc1ff85b44d1f6ff767969b3ee04f7
xxoome <akatsuki10086@gmail.com>
2017-06-12 15:10:19 +0800

    Create errback.py

--- Collector/spiders/errback.py
+++ Collector/spiders/errback.py
@@ -1,39 +1,3 @@
-# REDIRECT_PRIORITY_ADJUST
-# Default: +2
-# Scope: scrapy.downloadermiddlewares.redirect.RedirectMiddleware
-# Adjust redirect request priority relative to original request:
-# a positive priority adjust (default) means higher priority.
-# a negative priority adjust means lower priority.
-
-# RETRY_PRIORITY_ADJUST
-# Default: -1
-# Scope: scrapy.downloadermiddlewares.retry.RetryMiddleware
-# Adjust retry request priority relative to original request:
-# a positive priority adjust means higher priority.
-# a negative priority adjust (default) means lower priority.
-
-# priority (int) – the priority of this request (defaults to 0). 
-# The priority is used by the scheduler to define the order used to process requests. 
-# Requests with a higher priority value will execute earlier. Negative values are allowed in order to indicate relatively low-priority.
-
-# DEPTH_PRIORITY
-# Default: 0
-# Scope: scrapy.spidermiddlewares.depth.DepthMiddleware
-# An integer that is used to adjust the request priority based on its depth:
-# if zero (default), no priority adjustment is made from depth
-# a positive value will decrease the priority, i.e. higher depth requests will be processed later ; this is commonly used when doing breadth-first crawls (BFO)
-# a negative value will increase priority, i.e., higher depth requests will be processed sooner (DFO)
-# This setting adjusts priority in the opposite way compared to other priority settings REDIRECT_PRIORITY_ADJUST and RETRY_PRIORITY_ADJUST.
-
-# By default, Scrapy uses a LIFO queue for storing pending requests, which basically means that it crawls in DFO order. 
-# This order is more convenient in most cases. If you do want to crawl in true BFO order, you can do it by setting the following settings:
-# DEPTH_PRIORITY = 1
-# SCHEDULER_DISK_QUEUE = 'scrapy.squeues.PickleFifoDiskQueue'
-# SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.FifoMemoryQueue'
-
-# 一旦发生重定向,DUPEFILTER_CLASS会把所有重定向过的url都记录下来
-#只有当函数里面出现yield关键字时，response.meta才会有depth属性
-
 from scrapy import Spider,Request
 from scrapy.spidermiddlewares.httperror import HttpError
 from twisted.internet.error import TimeoutError, TCPTimedOutError,DNSLookupError,ConnectionRefusedError
