536b444a8a4fd6670f250840c47115953aef8c30
xxoome <akatsuki10086@gmail.com>
2017-08-03 11:14:00 +0800

    'delete'

--- data/weibo.sql
+++
@@ -1,7 +0,0 @@
-insert into tweet(content,publish_time,num_praise,title,news_id,num_relay,num_comment) values('转发理由:没想到，手游玩家最密集的20个城市中，河南省成了亮点。  ','2016-04-20 20:59:42','2','','M_DrSuB22q5','1357','91');
-insert into relay(news_id,relay_date,user_id,relays,num_praise) values('M_DrSGy7VMc','2016-04-20 21:35:43','/u/5774656000','转发微博 ','0');
-insert into attitude(news_id,praise_date,user_id) values('M_DrSGy7VMc','2016-04-20 21:35:43','/u/3198854631');
-insert into comment(comment_date,user_id,comment_id,comments,news_id,num_attitude) values('2016-04-20 21:35:44','/u/3745612693','C_3966452396495572','风向在变','M_DrSJ77s0L','0');
-insert into attitude(news_id,praise_date,user_id) values('M_DrSJ77s0L','2016-04-20 21:35:45','/u/3762806542');
-insert into attitude(news_id,praise_date,user_id) values('M_DrSJ77s0L','2016-04-20 21:35:45','/u/5487807233');
-commit;
