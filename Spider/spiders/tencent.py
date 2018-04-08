7d15f85d79d576e6d3f2526dcbf8c3af77cd7b2d
neos <akatsuki10086@gmail.com>
2018-04-08 11:58:11 +0800

    Update tencent.py

--- Spider/spiders/tencent.py
+++ Spider/spiders/tencent.py
@@ -8,7 +8,7 @@ class Tencent(CrawlSpider):
   }
   name = "tencent"
   allowed_domains = ["tencent.com"]
-  start_urls = ["http://hr.tencent.com/position.php"]
+  start_urls = ["https://hr.tencent.com/position.php"]
   rules =(
          Rule(
              LE(allow=("start=\d{,4}#a")),
@@ -21,15 +21,15 @@ class Tencent(CrawlSpider):
     print(response.url,response.meta['depth'])
     '''
     当depth_limit=1时打印如下:
-    http://hr.tencent.com/position.php?start=10 1
-    http://hr.tencent.com/position.php?start=20 1
-    http://hr.tencent.com/position.php?start=30 1
-    http://hr.tencent.com/position.php?start=40 1
-    http://hr.tencent.com/position.php?start=50 1
-    http://hr.tencent.com/position.php?start=60 1
-    http://hr.tencent.com/position.php?start=70 1
-    http://hr.tencent.com/position.php?start=1490 1
+    https://hr.tencent.com/position.php?start=10 1
+    https://hr.tencent.com/position.php?start=20 1
+    https://hr.tencent.com/position.php?start=30 1
+    https://hr.tencent.com/position.php?start=40 1
+    https://hr.tencent.com/position.php?start=50 1
+    https://hr.tencent.com/position.php?start=60 1
+    https://hr.tencent.com/position.php?start=70 1
+    https://hr.tencent.com/position.php?start=1490 1
     注意第一页的depth=2,他是通过depth=1的页面获得的
     再来看start_requests函数
-    这里为什么要用dont_filter=True ?(本例中start_urls[0]就被访问了2次，所以要用dont_filter=True来说明)
+    为什么要用dont_filter=True ?(本例中start_urls[0]就被访问了2次，所以要用dont_filter=True来说明)
     '''
