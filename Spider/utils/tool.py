9ebd96ffbe462e50e4b5f6093d634c1af0b33f0a
neos <akatsuki10086@gmail.com>
2018-06-04 09:55:11 +0800

    Update and rename v2ex.py to tool.py

--- Spider/utils/tool.py
+++ Spider/utils/tool.py
@@ -1,5 +1,4 @@
-# coding:utf-8
-import requests
+import requests,time,hashlib
 from lxml.html import fromstring
 
 class V2ex():
@@ -36,7 +35,35 @@ class V2ex():
     else:
       print('已经领取过每日登录奖励...')
 
+def translate(word):
+    s=requests.Session()
+    s.get('http://fanyi.youdao.com')   #作用是获取cookie
+    s.headers.update({                 #cookie,referer,user-agent缺一不可
+        'Referer':'http://fanyi.youdao.com/',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.39 Safari/537.36',
+    })
+    url = 'http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
+    data = {
+        'i':word,
+        'from':'AUTO',
+        'to':'AUTO',
+        'smartresult':'dict',
+        'client':'fanyideskweb',
+        'salt':str(int(time.time()*1000)),
+        'doctype':'json',
+        'version':'2.1',
+        'keyfrom':'fanyi.web',
+        'action':'FY_BY_REALTIME',
+        'typoResult':'false',
+    }
+    data['sign'] = hashlib.md5(bytes(data['client']+data['i']+data['salt']+"ebSeFb%=XZ%T[KZ)c(sy!",'utf-8')).hexdigest()
+    r=s.post(url=url,data=data,)
+    return r.json()
+  
 if __name__ == '__main__':
     foo=V2ex()
     foo.login()
     foo.sign()
+    
+    # print(translate('今天星期五，心情大好，吃完饭准备跟朋友去抓鱼'))
+
