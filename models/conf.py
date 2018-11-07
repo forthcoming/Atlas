e01bfaae6358e3b779d47267fc3517580612ba28
neos <akatsuki10086@gmail.com>
2018-11-07 15:13:48 +0800

    Update conf.py

--- models/conf.py
+++ models/conf.py
@@ -1,6 +1,8 @@
 # Flask
+import multiprocessing
+
 bind = "127.0.0.1:7000"
-workers = 3 
+workers = multiprocessing.cpu_count()*2 +1
 errorlog='./error.log'  # 提前建好目录
 accesslog='./access.log'
 loglevel='debug'
