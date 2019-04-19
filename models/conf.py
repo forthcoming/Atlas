e6024d69ae6c238065a21db032012b4f59d050de
neos <akatsuki10086@gmail.com>
2019-04-19 18:44:40 +0800

    Update conf.py

--- models/conf.py
+++ models/conf.py
@@ -2,7 +2,7 @@
 import multiprocessing
 
 bind = "127.0.0.1:7000"
-workers = multiprocessing.cpu_count()*2 +1
+workers = multiprocessing.cpu_count()*2 +1  # 子进程个数,gunicorn主进程不对外提供服务
 errorlog='./error.log'  # 提前建好目录
 accesslog='./access.log'
 loglevel='debug'
