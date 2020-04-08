71c588aab9161140a5d79791a82121549c45a3a5
neos <>
2020-04-09 00:01:22 +0800

    update

--- crontab.sh
+++
@@ -1,7 +0,0 @@
-path=$(cd `dirname $0`;pwd)
-cd $path/../../atlas/spider/bi_spider
-
-export LD_LIBRARY_PATH=/usr/lib/oracle/12.2/client64/lib:$LD_LIBRARY_PATH
-
-python bi2atlas.py
-python alibaba2final.py > spider.log 2>&1
--- spider/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir data &>/dev/null
-SCRAPY=`which scrapy`
-echo '已包含如下爬虫:'
-$SCRAPY list
-echo '请输入想要运行的爬虫名,如amazon'
-read NAME
-echo "The $NAME spider is running !"
-$SCRAPY crawl $NAME
